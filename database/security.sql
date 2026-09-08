89cde569b58156cdae321d661c2fee0d8a90b6d6
Ernesto Miranda Gil <dba@conacyt.mx>
2026-09-07 19:32:55 -0600

    Agrego estructuras y scripts a la prueba tecnica

--- database/security.sql
+++ database/security.sql
@@ -1,19 +1,19 @@
-Pues en mi caso yo definiria 3 perfiles:
+Pues en mi caso yo definiria  3  perfiles:
 
-1. Aplicación
+Aplicación
 
 El usuario utilizado por la aplicación tendrá únicamente los permisos necesarios para realizar las operaciones propias del sistema.
 No tendrá permisos para modificar la estructura de la base de datos.
 
 
-2. Consulta
+Consulta
 
 El usuario destinado a consulta tendrá permisos de solo lectura.Podrá consultar información necesaria para generar reportes operativos y estadísticos
 , pero no podrá insertar, modificar ni eliminar información.
 Para proteger información sensible de los residentes, se recomienda utilizar vistas que expongan únicamente los datos necesarios para los reportes.
 
 
-3. Administrador de base de datos
+Administrador (DBA)
 
 El perfil DBA tendrá los privilegios necesarios para administrar la base de datos, incluyendo mantenimiento, creación y modificación de objetos, 
 índices, procedimientos, roles y permisos.
